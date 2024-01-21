b898cb78a662d200a216b1ee1561916ed712a4cb
Ilya Safronov <IlyaSafronov18@gmail.com>
2024-01-22 00:56:11 +0300

    add toggle js

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+const tab_btns = document.querySelectorAll('[data-target-tab]');
+const tab_contents = document.querySelectorAll('.registration-form .content');
+
+tab_btns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    tab_btns.forEach((btn) => btn.classList.remove('active'));
+    tab_contents.forEach((btn) => btn.classList.remove('active'));
+
+    btn.classList.toggle('active');
+    document.querySelector(btn.dataset.targetTab).classList.add('active');
+  });
+});
+
+// ! btn-ripple
+
